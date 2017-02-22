719fb34b0db7e10a02256915722cb12e7edcf940
Omotola <omotola.babasola@yahoo.com>
2017-02-22 13:43:49 +0000

    Initial back end framework using Maven

---
+++ src/main/java/com/example/BlockVoteApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockVoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockVoteApplication.class, args);
+	}
+}
