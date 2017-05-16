19d9063de47d5368b8d96f5dc34b107d88a10abf
Omotola <omotola.babasola@yahoo.com>
2017-05-16 01:58:41 +0100

    TryElGamal contract additive tallying works. Next ZKP

--- DApp/GethInterface.js
+++ DApp/GethInterface.js
@@ -10,16 +10,13 @@ web3.eth.defaultAccount = web3.eth.accounts[0];
 
 console.log(web3.eth.defaultAccount);
 
-var electionABI = [{
+var tryElGamalABI = [{
     constant: false,
-    inputs: [{
-        name: "candidate",
-        type: "bytes32"
-    }],
-    name: "countVotes",
+    inputs: [],
+    name: "tally",
     outputs: [{
         name: "",
-        type: "uint8"
+        type: "int256"
     }],
     payable: false,
     type: "function"
@@ -34,35 +31,28 @@ var electionABI = [{
     constant: true,
     inputs: [{
         name: "",
-        type: "bytes32"
+        type: "uint256"
     }],
-    name: "voteTable",
+    name: "commitments",
     outputs: [{
-        name: "",
-        type: "uint8"
+        name: "a",
+        type: "int256"
+    }, {
+        name: "b",
+        type: "int256"
     }],
     payable: false,
     type: "function"
 }, {
     constant: false,
     inputs: [{
-        name: "candidate",
-        type: "bytes32"
+        name: "a",
+        type: "int256"
+    }, {
+        name: "b",
+        type: "int256"
     }],
-    name: "isValidCandidate",
-    outputs: [{
-        name: "",
-        type: "bool"
-    }],
-    payable: false,
-    type: "function"
-}, {
-    constant: false,
-    inputs: [{
-        name: "candidate",
-        type: "bytes32"
-    }],
-    name: "vote",
+    name: "sendCommitment",
     outputs: [],
     payable: false,
     type: "function"
@@ -70,178 +60,112 @@ var electionABI = [{
     constant: true,
     inputs: [{
         name: "",
-        type: "uint256"
+        type: "int256"
     }],
-    name: "candidateList",
+    name: "logTable",
     outputs: [{
         name: "",
-        type: "bytes32"
-    }],
-    payable: false,
-    type: "function"
-}, {
-    inputs: [{
-        name: "_candidateList",
-        type: "bytes32[]"
-    }],
-    payable: true,
-    type: "constructor"
-}];
-var electionAddress = "0x97ec7b30d6376cd74bfe24c16c0053f8f7f58272";
-var election = web3.eth.contract(electionABI).at(electionAddress);
-
-var blindElectionAddress = "0x62d83b8f5bdedd47a1ff4460b911d874c4ae7d7b";
-var blindElectionABI = [{
-    constant: false,
-    inputs: [{
-        name: "candidateIndex",
-        type: "uint256"
-    }],
-    name: "vote",
-    outputs: [],
-    payable: true,
-    type: "function"
-}, {
-    constant: false,
-    inputs: [{
-        name: "candidateIndex",
-        type: "uint256"
-    }, {
-        name: "secret",
-        type: "uint256"
-    }],
-    name: "revealVote",
-    outputs: [],
-    payable: false,
-    type: "function"
-}, {
-    constant: true,
-    inputs: [{
-        name: "",
-        type: "uint256"
-    }],
-    name: "candidates",
-    outputs: [{
-        name: "name",
-        type: "bytes32"
-    }, {
-        name: "votes",
-        type: "uint256"
-    }],
-    payable: false,
-    type: "function"
-}, {
-    constant: false,
-    inputs: [],
-    name: "kill",
-    outputs: [],
-    payable: false,
-    type: "function"
-}, {
-    constant: false,
-    inputs: [{
-        name: "voter",
-        type: "address"
-    }],
-    name: "giveRightToVote",
-    outputs: [],
-    payable: false,
-    type: "function"
-}, {
-    constant: true,
-    inputs: [{
-        name: "",
-        type: "address"
-    }],
-    name: "voters",
-    outputs: [{
-        name: "eligible",
-        type: "bool"
-    }, {
-        name: "voted",
-        type: "bool"
-    }, {
-        name: "vote",
-        type: "uint256"
-    }, {
-        name: "blindedVote",
-        type: "bytes32"
+        type: "int256"
     }],
     payable: false,
     type: "function"
 }, {
     constant: true,
     inputs: [],
-    name: "countVotes",
-    outputs: [{
-        name: "winningCandidate",
-        type: "uint256"
-    }],
-    payable: false,
-    type: "function"
-}, {
-    constant: true,
-    inputs: [],
-    name: "electionStart",
+    name: "G",
     outputs: [{
         name: "",
-        type: "uint256"
+        type: "int256"
     }],
     payable: false,
     type: "function"
 }, {
     constant: true,
     inputs: [],
-    name: "electionEnd",
+    name: "H",
     outputs: [{
         name: "",
-        type: "uint256"
-    }],
-    payable: false,
-    type: "function"
-}, {
-    constant: true,
-    inputs: [],
-    name: "chairPerson",
-    outputs: [{
-        name: "",
-        type: "address"
-    }],
-    payable: false,
-    type: "function"
-}, {
-    constant: true,
-    inputs: [],
-    name: "winnerName",
-    outputs: [{
-        name: "winnerName",
-        type: "bytes32"
+        type: "int256"
     }],
     payable: false,
     type: "function"
 }, {
     constant: false,
     inputs: [{
-        name: "_blindedVote",
-        type: "bytes32"
+        name: "base",
+        type: "int256"
+    }, {
+        name: "exponent",
+        type: "int256"
+    }, {
+        name: "modulus",
+        type: "int256"
     }],
-    name: "blindVote",
-    outputs: [],
+    name: "mpmod",
+    outputs: [{
+        name: "",
+        type: "int256"
+    }],
+    payable: false,
+    type: "function"
+}, {
+    constant: false,
+    inputs: [{
+        name: "a",
+        type: "int256"
+    }, {
+        name: "b",
+        type: "int256"
+    }, {
+        name: "s",
+        type: "int256"
+    }],
+    name: "revealCommitment",
+    outputs: [{
+        name: "",
+        type: "int256"
+    }],
+    payable: false,
+    type: "function"
+}, {
+    constant: true,
+    inputs: [],
+    name: "P",
+    outputs: [{
+        name: "",
+        type: "int256"
+    }],
+    payable: false,
+    type: "function"
+}, {
+    constant: false,
+    inputs: [{
+        name: "a",
+        type: "int256"
+    }],
+    name: "logG",
+    outputs: [{
+        name: "",
+        type: "int256"
+    }],
     payable: false,
     type: "function"
 }, {
     inputs: [{
-        name: "candidateNames",
-        type: "bytes32[]"
+        name: "_P",
+        type: "int256"
     }, {
-        name: "electionDuration",
-        type: "uint256"
+        name: "_G",
+        type: "int256"
+    }, {
+        name: "_H",
+        type: "int256"
     }],
     payable: false,
     type: "constructor"
 }];
-
-var blindElection = web3.eth.contract(blindElectionABI).at(blindElectionAddress);
+var tryElGamalAddress = "0xdb0590eb105b583bd1650e7accaddd3b11e97489";
+var tryElGamal = web3.eth.contract(tryElGamalABI).at(tryElGamalAddress);
 
 $(function() {
     $("#voteForm").on('submit', function(e) {
@@ -266,25 +190,33 @@ $(function() {
                 var random = 52;
                 commitment = generateElGamalCommitment(vote, random, params.P, params.G, params.H);
                 console.log(commitment);
+                tryElGamal.sendCommitment(commitment.a, commitment.b);
             });
 
-            //election.vote(candidate);
+
         });
     });
 });
 
+//Currently additive elGamal (G^r, G^m * H^r)
 function generateElGamalCommitment (candidate_id, random, P, G, H) {
     var generateMessage = 'generating commitment for candidate ';
     console.log(generateMessage.concat(candidate_id));
     var commitment = {a:0, b:0};
     commitment.a = mpmod(G, random, P);
-    commitment.b = mpmod(mpmod(candidate_id, 1, P) * mpmod(H, random, P), 1, P);
+    commitment.b = mpmod(mpmod(G, candidate_id, P) * mpmod(H, random, P), 1, P);
     return commitment;
 };
 
+
 function mpmod(base, exponent, modulus) {
-    if ((base < 1) || (exponent < 0) || (modulus < 1)) {
+    if ((base < 1) || (modulus < 1)) {
         return("invalid");
+    }
+    if (exponent < 0) {
+        while (exponent < 0) {
+            exponent += ((modulus - 1)/2);
+        }
     }
     result = 1;
     while (exponent > 0) {
@@ -296,4 +228,68 @@ function mpmod(base, exponent, modulus) {
     }
     return (result);
 }
+/*
+function revealCommitment(a, b, s) {
+    var As = mpmod(a, -s, P);
+    console.log(As);
+    var gm = mpmod(mpmod(b,1,P) * As, 1, P);
+    console.log(gm);
+    return logs[gm];
+}
 
+var logs = {
+    1 : 0,
+    34 : 1,
+    86 : 2,
+    35 : 3,
+    13 : 4,
+    14 : 5,
+    48 : 6,
+    27 : 7,
+    62 : 8,
+    75 : 9,
+    89 : 10,
+    30 : 11,
+    57 : 12,
+    12 : 13,
+    87 : 14,
+    69 : 15,
+    99 : 16,
+    49 : 17,
+    61 : 18,
+    41 : 19,
+    3 : 20,
+    102 : 21,
+    44 : 22,
+    105 : 23,
+    39 : 24,
+    42 : 25,
+    37 : 26,
+    81 : 27,
+    79 : 28,
+    11 : 29,
+    53 : 30,
+    90 : 31,
+    64 : 32,
+    36 : 33,
+    47 : 34,
+    100 : 35,
+    83 : 36,
+    40 : 37,
+    76 : 38,
+    16 : 39,
+    9 : 40,
+    92 : 41,
+    25 : 42,
+    101 : 43,
+    10 : 44,
+    19 : 45,
+    4 : 46,
+    29 : 47,
+    23 : 48,
+    33 : 49,
+    52 : 50,
+    56 : 51,
+    85 : 52
+};
+*/
